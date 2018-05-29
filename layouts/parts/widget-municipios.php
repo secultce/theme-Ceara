f75f00fd748c17635e28a58d354460a2d4f65501
nalomysouza <nalomysouza@gmail.com>
2018-05-28 21:23:20 -0300

    refatorando os widgets

--- layouts/parts/widget-municipios.php
+++ layouts/parts/widget-municipios.php
@@ -3,18 +3,18 @@ $entityClass = $entity->getClassName();
 $entityName = strtolower(array_slice(explode('\\', $entityClass),-1)[0]);
 $municipios = array_values($app->getRegisteredTaxonomy($entityClass, 'municipio')->restrictedTerms);
 sort($municipios);
+$selected = $entity->terms['municipio'];
 ?>
-<?php if($this->isEditable() || !empty($municipios)): ?>
+<?php if($this->isEditable() || !empty($selected)): ?>
 <div class="widget">
     <h3><?php echo "Municípios Contemplados" ?></h3>
     <?php if($this->isEditable()): ?>
-        <span id="term-area" class="js-editable-taxonomy" data-original-title="<?php echo "Municípios Contemplados" ?>" data-emptytext="<?php echo "Informe, pelo menos, um município." ?>" data-restrict="true" data-taxonomy="municipio"><?php echo implode('; ', $entity->terms['municipio'])?></span>
+        <span id="term-area" class="js-editable-taxonomy" data-original-title="<?php echo "Municípios Contemplados" ?>" data-emptytext="<?php echo "Informe, pelo menos, um município." ?>" data-restrict="true" data-taxonomy="municipio"><?php echo implode('; ', $selected)?></span>
     <?php else: ?>
-        <?php
-        foreach($municipios as $i => $t): if(in_array($t, $entity->terms['municipio'])): ?>
-            <a class="tag tag-<?php echo $this->controller->id ?>" href="<?php echo $app->createUrl('site', 'search') ?>##(<?php echo $entityName ?>:(areas:!(<?php echo $i ?>)),global:(enabled:(<?php echo $entityName ?>:!t),filterEntity:<?php echo $entityName ?>))">
+        <?php foreach($municipios as $i => $t): if(in_array($t, $selected)): ?>
+            <span class="tag tag-<?php echo $this->controller->id ?>">
                 <?php echo $t ?>
-            </a>
+            </span>
         <?php endif; endforeach; ?>
     <?php endif;?>
 </div>
--- layouts/parts/widget-publicos.php
+++ layouts/parts/widget-publicos.php
@@ -1,19 +1,21 @@
 <?php
 $entityClass = $entity->getClassName();
 $entityName = strtolower(array_slice(explode('\\', $entityClass),-1)[0]);
-$areas = array_values($app->getRegisteredTaxonomy($entityClass, 'publico')->restrictedTerms);
-sort($areas);
+$publicos_alvo = array_values($app->getRegisteredTaxonomy($entityClass, 'publico')->restrictedTerms);
+sort($publicos_alvo);
+$selected = $entity->terms['publico'];
 ?>
+<?php if($this->isEditable() || !empty($selected)): ?>
 <div class="widget">
     <h3><?php echo "Público-Alvo" ?></h3>
     <?php if($this->isEditable()): ?>
-        <span id="term-area" class="js-editable-taxonomy" data-original-title="<?php echo "Público-Alvo" ?>" data-emptytext="<?php echo "Selecione pelo menos um Público-Alvo" ?>" data-restrict="true" data-taxonomy="publico"><?php echo implode('; ', $entity->terms['publico'])?></span>
+        <span id="term-area" class="js-editable-taxonomy" data-original-title="<?php echo "Público-Alvo" ?>" data-emptytext="<?php echo "Informe, pelo menos, um Público-Alvo" ?>" data-restrict="true" data-taxonomy="publico"><?php echo implode('; ', $selected)?></span>
     <?php else: ?>
-        <?php
-        foreach($areas as $i => $t): if(in_array($t, $entity->terms['publico'])): ?>
-            <a class="tag tag-<?php echo $this->controller->id ?>" href="<?php echo $app->createUrl('site', 'search') ?>##(<?php echo $entityName ?>:(areas:!(<?php echo $i ?>)),global:(enabled:(<?php echo $entityName ?>:!t),filterEntity:<?php echo $entityName ?>))">
+        <?php foreach($publicos_alvo as $i => $t): if(in_array($t, $selected)): ?>
+            <a class="tag tag-<?php echo $this->controller->id ?>">
                 <?php echo $t ?>
             </a>
         <?php endif; endforeach; ?>
     <?php endif;?>
 </div>
+<?php endif; ?>
