7db205672926b62c73b3352478b7105478821435
Wylder Marcos Silva Menezes <wylder.marcos@secult.local>
2020-12-18 17:04:27 -0300

    em desenvolvimento bug do botão antigo

--- views/opportunity/report-old.php
+++ views/opportunity/report-old.php
@@ -3,63 +3,70 @@ use MapasCulturais\Entities\Registration as R;
 use MapasCulturais\Entities\Agent;
 use MapasCulturais\i;
 
-$app->disableAccessControl();
+
+try{
+    $app->disableAccessControl();
 
 
-function echoStatus($registration) {
-    switch ($registration->status){
-        case R::STATUS_APPROVED:
-            i::_e('selecionada');
-            break;
+    function echoStatus($registration) {
+        switch ($registration->status){
+            case R::STATUS_APPROVED:
+                i::_e('selecionada');
+                break;
 
-        case R::STATUS_NOTAPPROVED:
-            i::_e('não selecionada');
-            break;
+            case R::STATUS_NOTAPPROVED:
+                i::_e('não selecionada');
+                break;
 
-        case R::STATUS_WAITLIST:
-            i::_e('suplente');
-            break;
+            case R::STATUS_WAITLIST:
+                i::_e('suplente');
+                break;
 
-        case R::STATUS_INVALID:
-            i::_e('inválida');
-            break;
+            case R::STATUS_INVALID:
+                i::_e('inválida');
+                break;
 
-        case R::STATUS_SENT:
-            i::_e('pendente');
-            break;
+            case R::STATUS_SENT:
+                i::_e('pendente');
+                break;
+        }
     }
+
+    function showIfField($hasField, $showField) {
+        if($hasField)
+            echo "<th>" . $showField . "</th>";
+    }
+
+    $_properties = $app->config['registration.propertiesToExport'];
+    $custom_fields = [];
+    foreach($entity->registrationFieldConfigurations as $field) :
+        $custom_fields[$field->displayOrder] = [
+            'title' => $field->title,
+            'field_name' => $field->getFieldName()
+        ];
+    endforeach;
+
+    ksort($custom_fields);
+
+    $metas_individual=[];
+    $metas_coletivo=[];
+
+    $metas = $app->getRegisteredMetadata('MapasCulturais\Entities\Agent', 1);
+    foreach($metas as $metadata){
+        $metas_individual[] = $metadata->key;
+    }
+
+    $metas = $app->getRegisteredMetadata('MapasCulturais\Entities\Agent', 2);
+    foreach($metas as $metadata){
+        $metas_coletivo[] = $metadata->key;
 }
-
-function showIfField($hasField, $showField) {
-    if($hasField)
-        echo "<th>" . $showField . "</th>";
+}catch(Exception $e){
+    printf($e);
+    var_dump($e);
+    die();
 }
-
-$_properties = $app->config['registration.propertiesToExport'];
-$custom_fields = [];
-foreach($entity->registrationFieldConfigurations as $field) :
-    $custom_fields[$field->displayOrder] = [
-        'title' => $field->title,
-        'field_name' => $field->getFieldName()
-    ];
-endforeach;
-
-ksort($custom_fields);
-
-$metas_individual=[];
-$metas_coletivo=[];
-
-$metas = $app->getRegisteredMetadata('MapasCulturais\Entities\Agent', 1);
-foreach($metas as $metadata){
-    $metas_individual[] = $metadata->key;
-}
-
-$metas = $app->getRegisteredMetadata('MapasCulturais\Entities\Agent', 2);
-foreach($metas as $metadata){
-    $metas_coletivo[] = $metadata->key;
-}
-
 ?>
+
 <style>
     tbody td, table th{
         text-align: left !important;
@@ -104,7 +111,9 @@ foreach($metas as $metadata){
         </tr>
     </thead>
     <tbody>
+      
         <?php foreach($entity->sentRegistrations as $r): ?>
+            
             <tr>
                 <td><a href="<?php echo $r->singleUrl; ?>" target="_blank"><?php echo $r->number; ?></a></td>
                 <?php if($entity->projectName): ?>
@@ -119,18 +128,28 @@ foreach($metas as $metadata){
                 <?php showIfField($entity->registrationCategories, $r->category); ?>
 
                 <?php
-                foreach($custom_fields as $field):
-                    $_field_val = (isset($field["field_name"])) ? $r->{$field["field_name"]} : "";
-
-                    if(is_array($_field_val) && isset($_field_val[0]) && $_field_val[0] instanceof stdClass) {
-                        $_field_val = (array)$_field_val[0];
-                    }
-
-                    echo "<th>";
-                    echo (is_array($_field_val)) ? implode(", ", $_field_val) : $_field_val;
-                    echo "</th>";
-                    
+                
+                    foreach($custom_fields as $field):
+                        $_field_val = (isset($field["field_name"])) ? $r->{$field["field_name"]} : "";
+    
+                        if(is_array($_field_val) && isset($_field_val[0]) && $_field_val[0] instanceof stdClass) {
+                            $_field_val = (array)$_field_val[0];
+                            echo "<th>";
+                            echo (is_array($_field_val)) ? implode(", ", $_field_val) : $_field_val;
+                            echo "</th>";
+                        }
+                            // echo "<th>";
+                            // echo (is_array($_field_val)) ? implode(", ", $_field_val) : $_field_val;
+                            // echo "</th>"; 
+                        
+    
+                        // echo "<th>";
+                        // echo (is_array($_field_val)) ? implode(", ", $_field_val) : $_field_val;
+                        // echo "</th>";
+                        //$data= json_decode( json_encode($_field_val), true);
+                        
                     endforeach;
+                
                 ?>
 
                 <td>
@@ -162,7 +181,7 @@ foreach($metas as $metadata){
                             if (!in_array($prop,$mdata)) continue;
                             $val = $agent->$prop;                            
                         ?>
-                        <td>
+                       <td>
                             <?php
                                 if ($prop === 'location')
                                     echo (isset($val['latitude']) && isset($val['longitude'])) ? "{$val['latitude']},{$val['longitude']}" : '';
@@ -183,6 +202,14 @@ foreach($metas as $metadata){
                         ?>
                     <?php endif; ?>
                 <?php endforeach;  ?>
+                <!--Informações dos espaços cadastrados-->
+                <?php foreach($r->getSpaceData() as $field): ?>
+                    <?php if(is_array($field)): ?>
+                        <td><?php echo implode(', ', $field); ?></td>
+                    <?php else: ?>
+                        <td><?php echo $field; ?></td>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </tr>
         <?php endforeach; ?>
     </tbody>
