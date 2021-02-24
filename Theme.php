c5fb1fa8796621dbdd6c6107400def497dc60824
João Vitor Barbosa <contato.cavalcantebarbosa@gmail.com>
2021-02-24 11:44:43 -0300

    Adicionado mensagem na tela inicial

--- Theme.php
+++ Theme.php
@@ -1518,6 +1518,14 @@ class Theme extends BaseV1\Theme
 
         });
 
+        $app->hook('template(site.index.home-search):end', function () use ($app) {
+            $texto = "Ação do Governo do Estado do Ceará que tem por objetivo conceder aos trabalhadores e trabalhadoras do setor de eventos um auxílio financeiro. Faz parte de um pacote de ações para socorrer o setor de eventos no Estado em meio à pandemia da Covid-19. <br/><br/> O auxílio será pago em duas parcelas de R$ 500, mediante cadastro dos profissionais junto à Secretaria da Cultura do Estado. Cerca de 10 mil profissionais, como músicos, humoristas e técnicos de som, deverão ser beneficiados. Ao todo R$ 10 milhões serão investidos pelo Estado para transferência dessa renda. Estão inclusos músicos, humoristas, profissionais de circo, técnicos de som, luz e imagem, montadores de palcos, etc.";
+            $botao = "Solicite seu auxílio";
+            $titulo = "AUXÍLIO FINANCEIRO AOS PROFISSIONAIS DO SETOR DE EVENTOS";
+
+            $this->part('auxilioeventos/home-search', ['texto' => $texto, 'botao' => $botao, 'titulo' => $titulo]);
+        });
+        
         $app->hook('<<GET|POST>>(registration.remove)', function () use ($app) {
 
             $this->requireAuthentication();
