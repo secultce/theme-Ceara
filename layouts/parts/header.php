db2a194fd06bb7bb06f9a0432eb65b53ffea80f5
Ben Rainir <benrainir@gmail.com>
2021-03-01 20:47:15 -0300

    adicionando template hook no head

--- layouts/parts/header.php
+++ layouts/parts/header.php
@@ -5,10 +5,14 @@ $title = isset($entity) ? $this->getTitle($entity) : $this->getTitle()
 <!DOCTYPE html>
 <html lang="<?php echo $app->getCurrentLCode(); ?>" dir="ltr">
     <head>
+    
+        <?php $this->applyTemplateHook('head','begin'); ?>
+
         <meta charset="UTF-8" />
         <title><?php echo $title == $site_name ? $title : "{$site_name} - {$title}"; ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="shortcut icon" href="<?php $this->asset('img/favicon.ico') ?>" />
+        
         <?php $this->head(isset($entity) ? $entity : null); ?>
         <!--[if lt IE 9]>
         <script src="<?php $this->asset('js/html5.js'); ?>" type="text/javascript"></script>
