d98617494bd08c7caa06d7b47e18fb8a0bf53125
Ben Rainir <benrainir@gmail.com>
2021-02-28 22:21:38 -0300

    ajustes no footer adicionando hooks do BaseV1

--- layouts/parts/footer.php
+++ layouts/parts/footer.php
@@ -1,5 +1,12 @@
 </section>
-<footer id="main-footer"></footer>
+
+<?php $this->applyTemplateHook('main-footer','before'); ?>
+<footer id="main-footer">
+    <?php $this->applyTemplateHook('main-footer','begin'); ?>
+    <?php $this->applyTemplateHook('main-footer','end'); ?>
+</footer>
+<?php $this->applyTemplateHook('main-footer','after'); ?>
+
 <?php $this->part('templates'); ?>
 <?php $this->bodyEnd(); ?>
 <iframe id="require-authentication" src="" style="display:none; position:fixed; top:0%; left:0%; width:100%; height:100%; z-index:100000"></iframe>
@@ -9,15 +16,6 @@
         <img id="human-crop-image"/>
     </div>
 <?php endif; ?>
-<!-- <div vw class="enabled">
-    <div vw-access-button class="active"></div>
-    <div vw-plugin-wrapper>
-      <div class="vw-plugin-top-wrapper"></div>
-    </div>
-  </div>
-  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
-  <script>
-    new window.VLibras.Widget('https://vlibras.gov.br/app');
-  </script> -->
+
 </body>
 </html>
