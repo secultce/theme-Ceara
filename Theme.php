eb9cf3ebe25897571bd0ac27f66dc392a7066943
Ben Rainir <benrainir@gmail.com>
2021-03-11 13:25:41 -0300

    remover Aviso do Edital Auxilio Financeiro Setor de eventos

--- Theme.php
+++ Theme.php
@@ -1519,7 +1519,8 @@ class Theme extends BaseV1\Theme
         });
 
         $app->hook('template(site.index.home-search):begin', function () use ($app) {
-           
+           return;
+           /*
             $titulo = "AUXÍLIO FINANCEIRO AOS PROFISSIONAIS DO SETOR DE EVENTOS";
             $titulo_url =  $app->createUrl('opportunity','single', [2852]);
             $texto = "Ação do Governo do Estado do Ceará que tem por objetivo conceder aos trabalhadores e trabalhadoras do setor de eventos um auxílio financeiro. Faz parte de um pacote de ações para socorrer o setor de eventos no Estado em meio à pandemia da Covid-19. <br/><br/> O auxílio será pago em duas parcelas de R$ 500, mediante cadastro dos profissionais junto à Secretaria da Cultura do Estado, através do Mapa Cultural do Ceará. Cerca de 10 mil profissionais, como músicos, humoristas e técnicos de som, deverão ser beneficiados. Ao todo R$ 10 milhões serão investidos pelo Estado para transferência dessa renda. Estão inclusos músicos, humoristas, profissionais de circo, técnicos de som, luz e imagem, montadores de palcos, etc.";
@@ -1527,6 +1528,7 @@ class Theme extends BaseV1\Theme
             $botao_url = $app->createUrl('opportunity','single', [2852]);
 
             $this->part('auxilioeventos/home-search', ['texto' => $texto, 'botao' => $botao, 'titulo' => $titulo, 'titulo_url'=> $titulo_url, 'botao_url' => $botao_url]);
+            */
         });
         
         $app->hook('<<GET|POST>>(registration.remove)', function () use ($app) {
