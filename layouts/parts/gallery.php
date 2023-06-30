7c7bbcc7a204e3e648437a8440473485305d9c2d
Junior-Shyko <franciscaoanto@gmail.com>
2023-06-30 16:36:12 -0300

    :construction: Paginação de imagens
    Paginando imagens

--- layouts/parts/gallery.php
+++ layouts/parts/gallery.php
@@ -2,7 +2,34 @@
 if($this->controller->action === 'create')
     return;
 ?>
-<?php if(!is_object($entity)):?>
+
+<?php 
+$sql = "
+select * from public.file where object_id = 11854 
+";
+
+// Paginação
+$currentPage = $_GET['page'] ?? 1;
+$itemsPerPage = 50;
+$offset = ($currentPage - 1) * $itemsPerPage;
+$sql .= ' LIMIT ' . $itemsPerPage . ' OFFSET ' . $offset;
+echo $sql;
+$stmt = $app->em->getConnection()->prepare($sql);
+$stmt->execute();
+$results = $stmt->fetchAll();
+    // dump($results);
+    // foreach ($results as $image) {
+    //     dump($image);
+    //     // echo '<div class="clearfix js-gallery">
+    //     //     <div class="image-gallery-item" >
+    //     //     <a href="#" class="btn btn-primary">'.$image['md5'].'</a>
+    //     //     </div>
+    
+    //     // </div>';
+
+    // }
+
+if(!is_object($entity)):?>
     <div class="alert info"><?php MapasCulturais\i::__("Nenhuma imagem disponível");?></div>
     <?php return;?>
 <?php endif;?>
@@ -15,19 +42,27 @@ if($this->controller->action === 'create')
 <?php if ($this->isEditable() || $gallery): ?>
     <h3><?php \MapasCulturais\i::_e("Galeria");?></h3>
     <div class="clearfix js-gallery">
-    <?php if($gallery): foreach($gallery as $img): ?>
-            <div id="file-<?php echo $img->id ?>" class="image-gallery-item" >                
-                <a href="<?php echo $img->url; ?>" title="<?php echo htmlspecialchars($img->description, ENT_QUOTES); ?> - Cadastrado em <?php echo $img->createTimestamp->format('d/m/Y á\s H:i:s')?>">
-                    <img src="<?php echo $img->transform('gallery')->url; ?>" />
-                </a>
-
+    <?php if($gallery): 
+        foreach($results as $img): ?>
+            <div id="file-<?php echo $img['id'] ?>" class="image-gallery-item" >        
+               
+                <a href="https://mapacultural.secult.ce.gov.br/files/agent/11854/s%C3%A3o_jo%C3%A3o_liter%C3%A1rio_val_paraiso_(1).jpg"> 
+                    <img src="https://mapacultural.secult.ce.gov.br/files/agent/11854/s%C3%A3o_jo%C3%A3o_liter%C3%A1rio_val_paraiso_(1).jpg" class/>
+            </a>
                 <?php if($this->isEditable()): ?>
 
                     <a data-href="<?php echo $img->deleteUrl?>" data-target="#file-<?php echo $img->id ?>" class="btn btn-default delete hltip js-remove-item" data-hltip-classes="hltip-ajuda" title="<?php \MapasCulturais\i::esc_attr_e("Excluir");?>"></a>
 
                 <?php endif; ?>
             </div>
-        <?php endforeach; endif;?>
+        <?php endforeach; 
+
+
+echo '<a id="prev-page" href="?page=' . ($currentPage - 1) . '" class="btn btn-primary">Página anterior</a>&nbsp&nbsp';
+echo '<a id="next-page" href="?page=' . ($currentPage + 1) . '" class="btn btn-primary">Próxima página</a>';
+
+
+    endif;?>
     </div>
     <?php if($this->isEditable()): ?>
         <p class="gallery-footer">
