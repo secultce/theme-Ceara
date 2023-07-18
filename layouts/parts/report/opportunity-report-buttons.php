001fafb163db888417be12e9916558d1b2ae7447
jocelio <joceliotecinfor@gmail.com>
2023-07-18 16:34:27 -0300

    Inserindo load ao baixar planilha

--- layouts/parts/report/opportunity-report-buttons.php
+++ layouts/parts/report/opportunity-report-buttons.php
@@ -10,8 +10,32 @@ $route_btn_antigo = App::i()->createUrl('opportunity', 'reportOld', [$entity->id
 <!-- <a class="btn btn-default download btn-report-evaluation-documental"  ng-click="editbox.open('report-evaluation-documental-options', $event)" rel="noopener noreferrer">Imprimir Resultado</a> -->
 
 <!--Botão antigo-->
-<a class="btn btn-default download" href="<?php echo $route_btn_antigo ?>"><?php i::_e("Baixar inscritos"); ?>(Botão antigo)</a>
+<a class="btn btn-default download" id="Clique" href="<?php echo $route_btn_antigo ?>"><?php i::_e("Baixar inscritos"); ?>(Botão antigo)</a>
 
+<div id="" style="display: none;">
+    <div ng-if="data.findingRegistrations">
+        <h3 class="alert warning ">Aguarde para baixar a planilha</h3>
+        <img src="<?php $this->asset('img/spinner_192.gif') ?>" width="48">
+    </div>
+</div>
+
+<script>
+    $("#Clique").click(function() {
+        $("#escondido").css("display", "block");
+    });
+
+    //detectar se o browse está baixando algo
+    const download = browser_is_downloading();
+    if (download) {
+       
+    }
+</script>
+
+
+<div id="escondido" style="display: none;">
+    <h3 class="alert warning ">Aguarde para baixar a planilha</h3>
+    <img src="<?php $this->asset('img/spinner_192.gif') ?>" width="48">
+</div>
 
 <!-- Formulário -->
 <!-- <edit-box id="report-evaluation-documental-options" position="top" title="<?php i::esc_attr_e('Imprimir Resultado') ?>" cancel-label="Cancelar" close-on-cancel="true">
