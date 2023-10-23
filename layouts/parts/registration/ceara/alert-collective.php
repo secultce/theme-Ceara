0a21c9e24de750fea240a7ce3808235a05db6ff7
Junior-Shyko <franciscaoanto@gmail.com>
2023-10-23 11:24:35 -0300

    :children_crossing: Alertas para o coletivo
    Alerta para coletivo sem o endereço e alerta para carregar a página

--- layouts/parts/registration/ceara/alert-collective.php
+++ layouts/parts/registration/ceara/alert-collective.php
@@ -6,13 +6,15 @@
         background-color: #076d21 !important;
         cursor: pointer;
     }
-    .ui-pnotify-action-bar button:hover{
+
+    .ui-pnotify-action-bar button:hover {
         color: #fff;
         border: 1px solid #076d21 !important;
         border-radius: 3px !important;
         background-color: #519863 !important;
         cursor: pointer;
     }
+
     .btn-perfil-collective {
         color: #076d21;
         background-color: #edf4ef !important;
@@ -20,130 +22,122 @@
         cursor: pointer;
     }
 </style>
+
 <div ng-repeat="def in data.entity.registrationAgents" ng-if="def.use != 'dontUse'">
-<div ng-if="def.agentRelationGroupName === 'coletivo'">
-            agentRelationGroupName: {{def.agentRelationGroupName}}
-            <script>
-                    console.log('Atualiza os dados');
-                    //todos os relacionamentos de agentes
-                    let agentRelation = MapasCulturais.entity.registrationAgents;
-                    let collectiveId = 0; //iniciando com 0
-                    agentRelation.forEach(element => {
-                        //se tiver um grupo com o nome coletivo, busca o ai do agente
-                        if (element.agentRelationGroupName == 'coletivo') {
-                            collectiveId = element.agent.id; //alterando o valor inicial
-                        }
+   
+    <div ng-if="def.agentRelationGroupName == 'coletivo' && def.relationStatus == 1">    
+   
+        <script>
+            let agentRelation = MapasCulturais.entity.registrationAgents;
+            let collectiveId = 0; //iniciando com 0
+            agentRelation.forEach(element => {
+                //se tiver um grupo com o nome coletivo, busca o ai do agente
+                if (element.agentRelationGroupName == 'coletivo' && element.agent !== null) {
+                   
+                    console.log(element.agent)
+                    collectiveId = element.agent.id; //alterando o valor inicial
+                }
+            });
+         
+             //requisição para api buscando o pai do coletivo          
+             $.ajax({
+                type: "GET",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                url: MapasCulturais.baseURL + "api/agent/find?@select=id,name,parent,endereco&id=EQ(" + collectiveId + ")",
+                success: function(res) {
+                    //Para preencher o valor do pai do coletivo
+                    let parentCollective = 0
+                    let address = ""
+                    res.forEach(element => {
+                        parentCollective = element.parent
+                        address = element.endereco
                     });
-                    //requisição para api buscando o pai do coletivo          
-                    $.ajax({
-                        type: "GET",
-                        contentType: "application/json; charset=utf-8",
-                        dataType: "json",
-                        url: MapasCulturais.baseURL + "api/agent/find?@select=id,name,parent,endereco&id=EQ(" + collectiveId + ")",
-                        success: function(res) {
- //Para preencher o valor do pai do coletivo
- let parentCollective = 0
-                        let address = ""
-                        res.forEach(element => {
-                            parentCollective = element.parent
-                            address = element.endereco
-                        });
-                        console.log({parentCollective})
-                        //se o id do agente é o mesmo do pai do coletivo, verifica algumas situações
-                        if (MapasCulturais.userProfile.id == parentCollective) {
-
-                            let fields = MapasCulturais.entity.registrationFieldConfigurations
-                            fields.forEach(element => {
-                                //se tem um campo do tipo agente coletivo que seja de endereço e que estava sem valor
-                                if (
-                                    element.fieldType == 'agent-collective-field' &&
-                                    element.config.entityField == '@location' &&
-                                    element.unchangedFieldJSON == "null"
-                                ) {
-
-                                    if (address == null) {
+                    
+                    //se o id do agente é o mesmo do pai do coletivo, verifica algumas situações
+                    if (MapasCulturais.userProfile.id == parentCollective) {
+                        //Todos os campos da oportunidade
+                        let fields = MapasCulturais.entity.registrationFieldConfigurations
+                        fields.forEach(element => {
+                            //se tem um campo do tipo agente coletivo que seja de endereço e que estava sem valor
+                            if (
+                                element.fieldType == 'agent-collective-field' && 
+                                element.config.entityField == '@location' 
+                            ) {
+                                //Se o agente coletivo estiver sem endereço
+                                if (address == null) {
+                                    new PNotify({
+                                        title: 'Ops, está faltando o endereço!',
+                                        text: 'Seu coletivo está sem endereço. Você pode alterar o perfil, ou preenher na inscrição. O que deseja fazer?',
+                                        type: 'info',
+                                        icon: 'fas fa-circle-info',
+                                        hide: false,
+                                        confirm: {
+                                            confirm: true,
+                                            buttons: [{
+                                                    text: 'Inserir aqui',
+                                                    addClass: 'btn-conf-collective ',
+                                                    click: function(notice) {
+                                                        notice.remove();
+                                                    }
+                                                },
+                                                {
+                                                    text: 'Inserir no perfil',
+                                                    addClass: 'btn-perfil-collective',
+                                                    click: function(notice) {
+                                                        console.log()
+                                                        let url = MapasCulturais.createUrl('agente' , 132894)
+                                                        window.location.href = url
+                                                    }
+                                                },
+                                            ]
+                                        },
+                                        buttons: {
+                                            closer: false,
+                                            sticker: false
+                                        },
+                                        history: {
+                                            history: false
+                                        }
+                                    })
+                                    return;
+                                }else if(element.unchangedFieldJSON.length > 0){
+                                    //Verificação de existe valores dentro dos campos de endereço
+                                    const existAddress = JSON.parse(element.unchangedFieldJSON);
+                                   
+                                    if(existAddress == null){
+                                        //Alerta para usuário e recarregando a página
                                         new PNotify({
-                                            title: 'Ops, está faltando o endereço!',
-                                            text: 'Seu coletivo está sem endereço. Você pode alterar o perfil, ou preenher na inscrição. O que deseja fazer?',
+                                            title: 'Aguarde!',
+                                            text: 'Estamos buscando os dados do seu Coletivo',
+                                            icon: 'error',
                                             type: 'info',
-                                            icon: 'fas fa-circle-info',
-                                            hide: false,
-                                            // addclass: 'stack-modal',
-                                            // stack: {
-                                            //     'dir1': 'down',
-                                            //     'dir2': 'right',
-                                            //     'modal': true
-                                            // },
-                                            confirm: {
-                                                confirm: true,
-                                                buttons: [{
-                                                        text: 'Inserir aqui',
-                                                        addClass: 'btn-conf-collective ',
-                                                        click: function(notice) {
-                                                            notice.remove();
-                                                        }
-                                                    },
-                                                    {
-                                                        text: 'Inserir no perfil',
-                                                        addClass: 'btn-perfil-collective',
-
-                                                        click: function(notice) {
-                                                            notice.update({
-                                                                title: 'You\'ve Chosen a Side',
-                                                                text: 'You want mashed potatoes.',
-                                                                icon: true,
-                                                                type: 'info',
-                                                                hide: true,
-                                                                confirm: {
-                                                                    confirm: false
-                                                                },
-                                                                buttons: {
-                                                                    closer: true,
-                                                                    sticker: true
-                                                                }
-                                                            });
-                                                        }
-                                                    },
-                                                ]
-                                            },
-                                            buttons: {
-                                                closer: false,
-                                                sticker: false
-                                            },
-                                            history: {
-                                                history: false
+                                            shadow: true,
+                                            stack: {
+                                                "dir1": "down",
+                                                "dir2": "right",
+                                                "push": "bottom",
+                                                "spacing1": 25,
+                                                "spacing2": 25,
+                                                "context": $("body"),
+                                                "modal": true
                                             }
-                                        })
-                                        return;
+                                        });
+                                        let url = MapasCulturais.createUrl('../inscricao',MapasCulturais.entity.id)
+                                        window.location.href = url;
                                     }
-                                    //Alerta para usuário e recarregando a página
-                                    // new PNotify({
-                                    //     title: 'Aguarde!',
-                                    //     text: 'Estamos buscando os dados do seu Coletivo',
-                                    //     icon: 'error',
-                                    //     type: 'info',
-                                    //     shadow: true,
-                                    //     stack: {"dir1": "down", "dir2": "right", "push": "bottom", "spacing1": 25, "spacing2": 25, "context": $("body"), "modal": true}
-                                    // });
-                                    // let url = MapasCulturais.createUrl('../inscricao',MapasCulturais.entity.id)
-                                    // window.location.href = url;
+
                                 }
-                            });
+                                
+                            }
+                        });
 
-                        }
-                        }
-                    }).done(function(res) {
-                        console.log('done', res)
-                    }).fail(function(err) {
-                        console.log('fail', err)
-                    }).always(function(res) {
-                        console.log('always', res)
-                       
-                    })
-                </script>
-
-        </div>
-      <div ng-if="def.relationStatus < 0" class="js-registration-agent registration-agent" ng-class="{pending: def.relationStatus < 0}">
+                    }
+                }
+            })
+        </script>
+    </div>
+    <div ng-if="def.relationStatus < 0" class="js-registration-agent registration-agent" ng-class="{pending: def.relationStatus < 0}">
 
         <div class="registration-fieldset alert info" style="border-radius: 15px;">
             <h5 ng-if="def.relationStatus < 0" id="" style="margin-left: 10px;">
