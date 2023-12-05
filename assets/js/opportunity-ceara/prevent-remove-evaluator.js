a28d0d9a662afc8419afe79e2a8a359d762183fc
Ronny John <me@ronnyjohnti.dev>
2023-12-05 15:46:18 -0300

    :pencil2: Alterado texto do alerta

--- assets/js/opportunity-ceara/prevent-remove-evaluator.js
+++ assets/js/opportunity-ceara/prevent-remove-evaluator.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     const evaluationInfoElement = document.createElement('div')
     evaluationInfoElement.classList.add('alert', 'info')
     evaluationInfoElement.innerHTML = "<p><strong>Atenção!</strong></p>" +
-        "Ao excluir um avaliador, todas as <strong>avaliações</strong> dessa oportunidade realizadas por ele também <strong>serão excluídas</strong>."
+        "Ao excluir um avaliador, todas as <strong>avaliações</strong> dessa oportunidade realizadas por esse avaliador também <strong>serão excluídas</strong>."
 
     $('.registration-fieldset #status-info')[0].after(evaluationInfoElement)
 })
