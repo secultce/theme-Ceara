185669593e98dea75730f22e6242e06425f10278
Ronny John <me@ronnyjohnti.dev>
2024-02-06 14:42:34 -0300

    fix: Fixed out of memory limit on API query agents

---
+++ ApiOutputs/Json.php
@@ -0,0 +1,25 @@
+<?php
+use MapasCulturais\App;
+
+class Json extends \MapasCulturais\ApiOutput{
+    protected function getContentType() {
+        return 'application/json';
+    }
+
+    protected function _outputError($data) {
+        echo json_encode(['error' => true, 'data' => $data]);
+    }
+
+    protected function _outputArray(array $data, $singular_object_name = 'Entity', $plural_object_name = 'Entities') {
+        $app = App::i();
+        ini_set('memory_limit', '256M');
+        $app->response()->header('Access-Control-Allow-Origin', \MapasCulturais\App::i()->config['api.accessControlAllowOrigin']);
+        echo json_encode($data);
+    }
+
+    protected function _outputItem($data, $object_name = 'Entity') {
+        $app = App::i();
+        $app->response()->header('Access-Control-Allow-Origin', \MapasCulturais\App::i()->config['api.accessControlAllowOrigin']);
+        echo json_encode($data);
+    }
+}
