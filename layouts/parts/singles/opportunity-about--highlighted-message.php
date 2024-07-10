028269071eed3d7479239abf25ecb943874d2264
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2024-07-10 10:16:12 -0300

    feat: exibir mensagem com número máximo de vagas para inscrição na oportunidade

--- layouts/parts/singles/opportunity-about--highlighted-message.php
+++ layouts/parts/singles/opportunity-about--highlighted-message.php
@@ -1,9 +1,16 @@
 <div class="highlighted-message clearfix" id="opportunity-main-info">
-    <?php $this->applyTemplateHook('tab-about--highlighted-message','begin'); ?>
+    <?php $this->applyTemplateHook('tab-about--highlighted-message', 'begin'); ?>
 
-    <?php if($this->isEditable() || $entity->registrationFrom || $entity->registrationTo): ?>
+    <?php if ($this->isEditable() || $entity->registrationFrom || $entity->registrationTo) : ?>
         <?php $this->part('singles/opportunity-about--registration-dates', ['entity' => $entity]) ?>
     <?php endif; ?>
 
-    <?php $this->applyTemplateHook('tab-about--highlighted-message','end'); ?>
+    <!-- Mensagem não aparecerá para os agentes com permissão de editar a oportunidade -->
+    <?php if ($entity->registrationLimit && !$entity->canUser('@control')) : ?>
+        <div style="margin-top: 32px;">
+            <?php \MapasCulturais\i::_e("Número máximo de vagas: "); ?><b><?php echo $entity->registrationLimit; ?></b>
+        </div>
+    <?php endif; ?>
+
+    <?php $this->applyTemplateHook('tab-about--highlighted-message', 'end'); ?>
 </div>
