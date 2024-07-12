40c735a31b3763271fadf5d844dec9fed386cfeb
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2024-07-12 15:02:04 -0300

    fix: melhoria de código nas mensagens da aba 'Principal' na oportunidade

--- layouts/parts/singles/opportunity-about--highlighted-message.php
+++ layouts/parts/singles/opportunity-about--highlighted-message.php
@@ -1,3 +1,7 @@
+<?php
+$registration_limit = (int) $entity->registrationLimit;
+?>
+
 <div class="highlighted-message clearfix" id="opportunity-main-info">
     <?php $this->applyTemplateHook('tab-about--highlighted-message', 'begin'); ?>
 
@@ -6,9 +10,9 @@
     <?php endif; ?>
 
     <!-- Mensagem não aparecerá para os agentes com permissão de editar a oportunidade -->
-    <?php if ($entity->registrationLimit && !$entity->canUser('@control')) : ?>
+    <?php if ($registration_limit && !$entity->canUser('@control')) : ?>
         <div style="margin-top: 32px;">
-            <?php \MapasCulturais\i::_e("Número máximo de vagas (inscrições): "); ?><b><?php echo $entity->registrationLimit; ?></b>
+            <?php \MapasCulturais\i::_e("Número máximo de vagas (inscrições): "); ?><b><?php echo $registration_limit; ?></b>
         </div>
     <?php endif; ?>
 
